93466c60dc77945be3dd33b065a9cc6600c225a2
yafimm <>
2022-02-04 09:52:55 +0700

    Add Frontend assets and Auth

---
+++ resources/views/layouts/_sidebar.blade.php
@@ -0,0 +1,40 @@
+<aside id="sidebar" class="sidebar">
+
+  <ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+      <a class="nav-link" href="index.html">
+        <i class="bi bi-grid"></i>
+        <span>Dashboard</span>
+      </a>
+    </li>
+
+    <li class="nav-heading">Master</li>
+
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="index.html">
+        <i class="bi bi-grid"></i>
+        <span>Medicine</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="index.html">
+        <i class="bi bi-grid"></i>
+        <span>Signa</span>
+      </a>
+    </li>
+
+    <li class="nav-heading">Master</li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="index.html">
+        <i class="bi bi-grid"></i>
+        <span>Prescription</span>
+      </a>
+    </li>
+
+  </ul>
+
+</aside><!-- End Sidebar-->
